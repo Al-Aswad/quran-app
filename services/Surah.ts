5769f1bd563b91f3cfe7f28fc2ee3e5a73212d84
Hajar Aswad <hajaraswadkom@gmail.com>
2022-08-09 09:07:30 +0700

    Deploy to vercel

--- services/Surah.ts
+++ services/Surah.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const base_url=process.env.NEXT_PUBLIC_BASE_URL;
+
 export const getListSurah=async()=>{
-    const res =await axios.get("https://equran.id/api/surat");
+    const res =await axios.get(`${base_url}/api/surat`);
     return res;
 }
 
